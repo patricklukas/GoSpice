d3f87814a127d9854df16a96e3ed473cff1d3203
Patrick Hein <patrick.hein@dwins.de>
2024-01-19 11:30:30 +0100

    aadd rook moves

--- engine/movegen.go
+++ engine/movegen.go
@@ -2,7 +2,11 @@ package engine
 
 // todo: generate enemy attacks
 // todo: calculate checkmask
+// todo: generate pin masks -> raycast from king to friendly pieces then raycast from friendly pieces to enemy pieces, if the raycast hits an enemy rook, bishop or queen, our piece is pinned
 // todo: check attacks blocking castling
+// todo: generate pawn movess
+// todo: generate queen moves
+// todo: generate knight moves
 
 // generate all possible king moves
 func GenKingMoves(brd Board) []Move {
@@ -53,6 +57,34 @@ func GenBishopMoves(brd Board) []Move {
 		for quiet != 0 {
 			to := popLsb(&quiet)
 			moveList = append(moveList, NewMoveQuiet(sq, to, BISHOP))
+		}
+	}
+	return moveList
+}
+
+func GenRookMoves(brd Board) []Move {
+	var moveList []Move
+	// locate our rooks
+	rooks := brd.pieces[ROOK] & brd.friendly()
+	// for each rook
+	for rooks != 0 {
+		// todo: handle pins
+		// todo: will use pinmasks and only allow rook moves along the pin
+		// get its square
+		sq := popLsb(&rooks)
+		// generate moves via raycasting
+		moves := generateRookAttacks(brd.occupied(), sq) & brd.checkmask
+		// handle captures
+		captures := moves & brd.enemy()
+		for captures != 0 {
+			to := popLsb(&captures)
+			moveList = append(moveList, NewMove(sq, to, ROOK, brd.pieceAt(to), EMPTY))
+		}
+		// handle quiet moves
+		quiet := moves & brd.empty()
+		for quiet != 0 {
+			to := popLsb(&quiet)
+			moveList = append(moveList, NewMoveQuiet(sq, to, ROOK))
 		}
 	}
 	return moveList
